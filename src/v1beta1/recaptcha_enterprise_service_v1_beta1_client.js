cd72bb5702a9072cb5528057d0f6aecdc5a72d64
yoshi-automation <yoshi-automation@google.com>
2019-06-04 12:40:10 -0700

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- src/v1beta1/recaptcha_enterprise_service_v1_beta1_client.js
+++ src/v1beta1/recaptcha_enterprise_service_v1_beta1_client.js
@@ -57,12 +57,15 @@ class RecaptchaEnterpriseServiceV1Beta1Client {
   constructor(opts) {
     this._descriptors = {};
 
+    const servicePath =
+      opts.servicePath || opts.apiEndpoint || this.constructor.servicePath;
+
     // Ensure that options include the service address and port.
     opts = Object.assign(
       {
         clientConfig: {},
         port: this.constructor.port,
-        servicePath: this.constructor.servicePath,
+        servicePath,
       },
       opts
     );
@@ -152,6 +155,14 @@ class RecaptchaEnterpriseServiceV1Beta1Client {
    * The DNS address for this API service.
    */
   static get servicePath() {
+    return 'recaptchaenterprise.googleapis.com';
+  }
+
+  /**
+   * The DNS address for this API service - same as servicePath(),
+   * exists for compatibility reasons.
+   */
+  static get apiEndpoint() {
     return 'recaptchaenterprise.googleapis.com';
   }
 
