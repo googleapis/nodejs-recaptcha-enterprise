75ee7216f778c9b4ae8d105948336f8dfc2dbf29
Benjamin Coe <bencoe@google.com>
2020-06-16 16:29:14 -0700

    chore: fix ordering of generation

--- system-test/fixtures/sample/src/index.js
+++ system-test/fixtures/sample/src/index.js
@@ -20,7 +20,7 @@
 const recaptchaenterprise = require('@google-cloud/recaptcha-enterprise');
 
 function main() {
-  const recaptchaEnterpriseServiceV1Beta1Client = new recaptchaenterprise.RecaptchaEnterpriseServiceV1Beta1Client();
+  const recaptchaEnterpriseServiceClient = new recaptchaenterprise.RecaptchaEnterpriseServiceClient();
 }
 
 main();
